e222f4d6d5ac1b6606cd3286181c16ffbb9d1fb0
benguz <84636906+benguz@users.noreply.github.com>
2021-07-07 13:25:43 -0400

    Update main.py

--- finesse/main.py
+++ finesse/main.py
@@ -1,7 +1,16 @@
 
 import fasttext
 
-model = fasttext.load_model("/Users/alex/result/cc.en.300.bin")
+import numpy as np
+
+# cosine similarity of word vectors
+
+from numpy import dot
+from numpy.linalg import norm
+
+# cos_sim = dot(a, b)/(norm(a)*norm(b))
+
+model = fasttext.load_model("/Users/benja/downloads/cc.en.300.bin.gz")
 
 
 ## next steps?: preprocess recommendations on some 30k most common words
@@ -22,58 +31,62 @@ model = fasttext.load_model("/Users/alex/result/cc.en.300.bin")
 class Finesse:
 
     #
-    def __init__(self, words, arr):
+    def __init__(self, words, arr, d):
         self.words = [] # list of words as strings
         self.arr = []   # list of vectors of words
+        self.d = {}     # part of speech dictionary
 
 
 
     # user word
 
-    def find_synonyms(self, word):
+    def find_synonyms(self, cos_sim, user_word_vec, user_word_pos):
+        # cos_sim should be 0.4 for this normalization method
+        syn_list = []
 
-        synList = []
+        current = user_word_vec / (norm(user_word_vec) * norm(self.arr))
 
+        # code from https://stackoverflow.com/questions/20825990/find-multiple-maximum-values-in-a-2d-array-fast
+        # for finding the num_largest maxima in a numpy array
 
+        num_largest = 15
+        full = np.dot(self.arr, current)
 
-        results = {}
-
-        # iterate through numpy array and find vectors that meet a threshold for cosine similarity
-        cos_sim = 0.4
-        for i in range(0, 29153):
-            current = dot(user_word_vec, arr[i]) / (norm(user_word_vec) * norm(arr[i]))
-            if current > cos_sim and words[i] != user_word and user_word[0:2] != words[i][0:2] and user_word[0:3] not in \
-                    words[i]:
-                results[words[i]] = current
+        indices = full.argpartition(full.size - num_largest, axis=None)[-num_largest:]
 
 
-
-        # dictionary: results
-        sorted_values = sorted(results.values(), reverse=True)  # Sort the values
-        sorted_dict = {}
-
-        for i in sorted_values:
-            for k in results.keys():
-                if results[k] == i:
-                    sorted_dict[k] = results[k]
-                    break
-
-        # print(sorted_dict)
         count = 0
-        for key in sorted_dict:
-            try:
-                if input_sent_pos[word_index][1] in d[key]:
-                    synList.append(key)
-                    count += 1
-                if count == 5:
-                    break
 
+        for i in range(num_largest-1, -1, -1):
+            if count == 5 or full[indices[i]] < cos_sim:
+                break
+
+            try:
+                if user_word_pos in self.d[self.words[indices[i]]]:
+                     synList.append(self.words[indices[i]])
+                     count += 1
+
+            except:
+                continue
+            # in the future, to fix the broad except statement,
+            # we may want to remove excess words from words[]
+
+
+
+
+
+        # for key in sorted_dict:
+        #     try:
+        #         if input_sent_pos[word_index][1] in d[key]:
+        #             synList.append(key)
+        #             count += 1
+        #         if count == 5:
+        #             break
+        #
 
         # print("---")
 
-        return synList
-
-
+        return syn_list
 
     def sentence_suggestions(self, input_sent):
         parser = CoreNLPParser(url='http://localhost:9000')
@@ -86,14 +99,13 @@ class Finesse:
         output_list = []
 
         for word_index in range(len(sent_length)):
-
-
             user_word = input_sent_pos[word_index][0]
             # print("Word: " + user_word + " POS: " + input_sent_pos[word_index][1])
 
             user_word_vec = model.get_word_vector(user_word)
+            user_word_pos = input_sent_pos[word_index][1]
 
-            output_list.append(find_synonyms(input_sent[word_index], user_word, user_word_vec))
+            output_list.append(find_synonyms(0.4, user_word_vec, user_word_pos))
 
 
 
