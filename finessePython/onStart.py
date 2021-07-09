eaf51d5b754247d38f99bc14c32ac50d9562980b
apesendorfer <alex.pesendorfer@gmail.com>
2021-07-09 11:52:45 -0400

    Create new python project folder with updated code

---
+++ finessePython/onStart.py
@@ -0,0 +1,13 @@
+
+import fasttext
+
+import numpy as np
+
+# cosine similarity of word vectors
+
+from numpy import dot
+from numpy.linalg import norm
+
+from nltk.parse import CoreNLPParser
+
+import timeit
