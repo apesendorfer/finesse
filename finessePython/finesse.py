065c8846559ebe216a9403512d2b9f93e1c4fccc
apesendorfer <alex.pesendorfer@gmail.com>
2021-07-15 14:05:42 -0400

    Update finesse.py

--- finessePython/finesse.py
+++ finessePython/finesse.py
@@ -71,49 +71,6 @@ class Finesse:
         return output_list
 
 start = timeit.default_timer()
-# Set of words from combined TF-IDF analysis results
-with open('word_list.txt', 'r') as fle:
-    data = fle.read()
-words = data.split(", ")
-stop = timeit.default_timer()
-print('Word List Time: ', stop - start)
-
-
-start = timeit.default_timer()
-# Numpy array to store word vectors
-arr = np.ndarray(shape=(29153, 300))
-
-# Store word vectors for the 29153 words in numpy array
-# Normalize all vectors when filling the array
-for i in range(0, 29153):
-    vec = model.get_word_vector(words[i])
-    arr[i] = np.array(vec)
-    arr[i] = arr[i] / norm(arr[i])
-
-stop = timeit.default_timer()
-print('Vector Array Time: ', stop - start)
-
-# with open('arr.txt', 'w') as file:
-#     file.write(str(arr))
-
-# IMPORT DICTIONARY FROM TEXT FILE
-
-# Using code from https://www.kite.com/python/answers/how-to-read-a-dictionary-from-a-file-in--python
-
-import ast
-start = timeit.default_timer()
-with open('pos_dict.txt', 'r') as fle:
-    contents = fle.read()
-dictionary = ast.literal_eval(contents)
-stop = timeit.default_timer()
-print('Dictionary Time: ', stop - start)
-
-start = timeit.default_timer()
-fin = Finesse(words, arr, dictionary)
-stop = timeit.default_timer()
-print('Instantiate Finesse Object: ', stop - start)
-
-start = timeit.default_timer()
 print(fin.sentence_suggestions("What a wonderful day!"))
 stop = timeit.default_timer()
 print('Short sentence suggestions: ', stop - start)
