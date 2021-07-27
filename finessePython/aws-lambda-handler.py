73faffb021764e1365ef069a083c30c91f25ce28
apesendorfer <alex.pesendorfer@gmail.com>
2021-07-27 17:15:23 -0400

    Update aws-lambda-handler.py

--- finessePython/aws-lambda-handler.py
+++ finessePython/aws-lambda-handler.py
@@ -16,7 +16,7 @@ def lambda_handler(event, context):
         "s3", aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_ACCESS_KEY
     )
     # Load models
-    model = fasttext.load_model(client.get_object(Bucket=BUCKET, Key="cc.en.300.bin"))
+    # model = fasttext.load_model(client.get_object(Bucket=BUCKET, Key="cc.en.300.bin"))
     nlp = spacy.load("en_core_web_sm")
     
     # Load word list: set of words from combined TF-IDF analysis results
@@ -33,6 +33,14 @@ def lambda_handler(event, context):
     with open(tfidf_word_vectors_fle, 'rb') as f:
         arr = np.load(f)
 
+    # Load 235892 word set
+    all_word_list_fle = client.get_object(Bucket=BUCKET, Key="total_word_list.json")
+    all_words = json.loads(all_word_list_fle.read().decode())
+
+    all_word_vectors_fle = client.get_object(Bucket=BUCKET, Key="all_word_vectors.npy")
+    with open(all_word_vectors_fle, 'rb') as f:
+        all_vecs = np.load(f)
+
     dict_fle = client.get_object(Bucket=BUCKET, Key="part_of_speech_lookup.json")
     dictionary = json.loads(dict_fle.read().decode())
 
@@ -47,31 +55,55 @@ def lambda_handler(event, context):
         input_sent_pos.append(token.tag_)
     output_list = []
     for word_index in range(len(input_sent)):
-        user_word = input_sent[word_index].lower()
-        user_word_vec = model.get_word_vector(user_word)
-        user_word_pos = input_sent_pos[word_index]
-        syn_list = []
-        current = user_word_vec / norm(user_word_vec)
-        # code from https://stackoverflow.com/questions/20825990/find-multiple-maximum-values-in-a-2d-array-fast
-        # for finding the num_largest maxima in a numpy array
-        num_largest = 15
-        full = np.dot(arr, current)
-        indices = full.argpartition(full.size - num_largest, axis=None)[-num_largest:]
-        count = 0
-        for i in range(num_largest-1, -1, -1):
-            if count == 5 or full[indices[i]] < COS_SIM:
-                break
+        # Find index of word in all_words list
+        if input_sent[word_index] in all_words or input_sent[word_index].lower() in all_words:
             try:
-                if user_word_pos in dictionary[words[indices[i]]] and user_word[0:3] not in words[indices[i]]:
-                    syn_list.append(words[indices[i]])
-                    count += 1
+                all_words_index = all_words.index(input_sent[word_index])
             except:
-                continue
+                all_words_index = all_words.index(input_sent[word_index].lower())
+            user_word = input_sent[word_index].lower()
+            # Retrieve corresponding word vector from all_vecs nparray
+            user_word_vec = all_vecs[all_words_index]
+            # user_word_vec = model.get_word_vector(user_word)
+            user_word_pos = input_sent_pos[word_index]
+            syn_list = []
+            current = user_word_vec / norm(user_word_vec)
+            # code from https://stackoverflow.com/questions/20825990/find-multiple-maximum-values-in-a-2d-array-fast
+            # for finding the num_largest maxima in a numpy array
+            num_largest = 15
+            full = np.dot(arr, current)
+            indices = full.argpartition(full.size - num_largest, axis=None)[-num_largest:]
+            count = 0
+            for i in range(num_largest-1, -1, -1):
+                if count == 5 or full[indices[i]] < COS_SIM:
+                    break
+                try:
+                    if user_word_pos in dictionary[words[indices[i]]] and user_word[0:3] not in words[indices[i]]:
+                        syn_list.append(words[indices[i]])
+                        count += 1
+                except:
+                    continue
 
-        output_list.append(syn_list)
+            output_list.append(syn_list)
+        else:
+            output_list.append([])
 
-    return {
-        'statusCode': 200,
-        'input_list': json.dumps(input_sent),
-        'output_list': json.dumps(output_list)
-    }
+    origin = event['headers']['origin']
+
+    if origin == 'https://apesendorfer.github.io':
+        response = {
+            "statusCode": 200,
+            "input_list": json.dumps(input_sent),
+            "output_list": json.dumps(output_list),
+            'headers': {
+                'Access-Control-Allow-Origin': origin,
+                # 'Access-Control-Allow-Credentials' : True,
+            },
+        }
+        return response
+    else:
+        response = {
+            "statusCode": 500,
+        }
+        return response
+    
